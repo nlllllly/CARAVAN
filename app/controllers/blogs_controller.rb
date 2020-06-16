9d6e8de03d35c7fabd8ab2f0342ba796ebba72cd
vagrant <vagrant@localhost.localdomain>
2020-06-16 05:33:24 +0000

    [update]CARAVAN

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,19 +1,20 @@
 class BlogsController < ApplicationController
   def index
+    @blogs = Blog.all
   end
 
   def show
+    @blog = Blog.find(params[:id])
   end
 
   def new
     @blog = Blog.new
-    
   end
 
   def create
-    blog = Blog.new(blog_params)
+    blog = Blog.new(blogs_params)
     blog.save
-    redirect_to blogs_path
+    redirect_to blog_path(blog.id)
 
   end
 
